849a29ba9b193e258896f6c2674042dd02c8403c
FEELTHEVENOM <rahul.valli2003@gmail.com>
2023-09-01 10:15:07 +0530

    nav change

---
+++ scripts/footer.js
@@ -0,0 +1,21 @@
+foot = `<footer>
+<div class="address">
+    <h1>IETE Chennai</h1>
+    <h3>Address:</h3>
+    <p>IETE Chennai Centre,<br> No. 37,
+    Conran Smith Road Entrance Peters Road,<br>
+    Gopalapuram, Chennai - 600 086,<br> Tamil Nadu, India.<br> <br>
+    Phone: 044 2835 0773 <br>
+    Email: ietechennai@gmail.com <br></p><br>
+    <p>© Copyright IETE Chennai. All Rights Reserved</p>
+</div>
+
+<div class="uselink">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+</div>      
+</footer> `
+document.write(foot)
---
+++ scripts/navbar.js
@@ -0,0 +1,27 @@
+nav = `
+<div class="header">
+<a href="#" class="logo"><img src="./src/logo.png" alt="Logo" class="logoimg"></a>
+<input type="checkbox" id="check">
+<label for="check" class="icons">
+    <i class='bx bx-menu' id="menu-icon"></i>
+    <i class='bx bx-x' id="close-icon"></i>
+</label>
+<a href ="#" id="iete">IETE</a>
+<p><b>The Institution of Electronics and Telecommunication Engineers,<br>Chennai</b></p>
+
+<!-- Navigation Bar -->
+<nav class="navbar">
+            <a href="../index.html">Home</a>
+            <a href="./about.html">About</a>
+            <a href="./execommit.html">Executive Committee</a>
+            <a href="./events.html">Events</a>
+            <a href="./membership.html">Membership</a>
+            <a href="#">Chencon</a>
+            <a href="#">Chenlink Newsletter</a>
+            <a href="./contact.html">Contact</a>
+        </nav>      
+</div>
+
+`
+
+document.write(nav)
