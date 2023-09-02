52197c398b152d2b30d79c62b9d4522c4dc4a461
FEELTHEVENOM <rahul.valli2003@gmail.com>
2023-09-02 15:59:38 +0530

    footer update 3

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,81 +1,47 @@
-foot = `
-<footer class="footer section">
-    <div class="footer__container container grid">
-        <div class="footer__content grid">
-            <div class="footer__data">
-                <h3 class="footer__title">Travel</h3>
-                <p class="footer__description">Travel you choose the <br> destination, 
-                    we offer you the <br> experience.
-                </p>
-                <div>
-                    <a href="https://www.facebook.com/" target="_blank" class="footer__social">
-                        <i class="ri-facebook-box-fill"></i>
-                    </a>
-                    <a href="https://twitter.com/" target="_blank" class="footer__social">
-                        <i class="ri-twitter-fill"></i>
-                    </a>
-                    <a href="https://www.instagram.com/" target="_blank" class="footer__social">
-                        <i class="ri-instagram-fill"></i>
-                    </a>
-                    <a href="https://www.youtube.com/" target="_blank" class="footer__social">
-                        <i class="ri-youtube-fill"></i>
-                    </a>
-                </div>
-            </div>
-
-            <div class="footer__data">
-                <h3 class="footer__subtitle">About</h3>
-                <ul>
-                    <li class="footer__item">
-                        <a href="" class="footer__link">About Us</a>
-                    </li>
-                    <li class="footer__item">
-                        <a href="" class="footer__link">Features</a>
-                    </li>
-                    <li class="footer__item">
-                        <a href="" class="footer__link">New & Blog</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="footer__data">
-                <h3 class="footer__subtitle">Company</h3>
-                <ul>
-                    <li class="footer__item">
-                        <a href="" class="footer__link">Team</a>
-                    </li>
-                    <li class="footer__item">
-                        <a href="" class="footer__link">Plan y Pricing</a>
-                    </li>
-                    <li class="footer__item">
-                        <a href="" class="footer__link">Become a member</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="footer__data">
-                <h3 class="footer__subtitle">Support</h3>
-                <ul>
-                    <li class="footer__item">
-                        <a href="" class="footer__link">FAQs</a>
-                    </li>
-                    <li class="footer__item">
-                        <a href="" class="footer__link">Support Center</a>
-                    </li>
-                    <li class="footer__item">
-                        <a href="" class="footer__link">Contact Us</a>
-                    </li>
-                </ul>
+foot = `<footer class="footer" style="margin-top: 50px;">
+<div class="container">
+    <h1>IETE Chennai</h1>
+    <div class="row">
+        <div class="footer-col">
+          
+            <h4>Address:</h4>
+            <p>IETE Chennai Centre,<br> No. 37,
+            Conran Smith Road Entrance Peters Road,<br>
+            Gopalapuram, Chennai - 600 086,<br> Tamil Nadu, India.<br> <br>
+            Phone: 044 2835 0773 <br>
+            Email: ietechennai@gmail.com <br></p><br>
+            
+        </div>
+        <div class="footer-col">
+            <h4>Nav Links</h4>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Committee</a></li>
+                <li><a href="#">Events</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>Courses</h4>
+            <ul>
+                <li><a href="#">watch</a></li>
+                <li><a href="#">bag</a></li>
+                <li><a href="#">shoes</a></li>
+                <li><a href="#">dress</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>follow us</h4>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a>
             </div>
         </div>
-
-        <div class="footer__rights">
-            <p class="footer__copy">&#169; 2021 Bedimcode. All rigths reserved.</p>
-            <div class="footer__terms">
-                <a href="#" class="footer__terms-link">Terms & Agreements</a>
-                <a href="#" class="footer__terms-link">Privacy Policy</a>
-            </div>
-        </div>
-    </div>
+    </div><br>
+    <p style="color:rgb(119, 119, 118);">© Copyright IETE Chennai. All Rights Reserved</p>
+</div>
 </footer>`
 document.write(foot)
