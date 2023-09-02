293ce5383f141f0909637e67e2eefd7fec247ef7
FEELTHEVENOM <rahul.valli2003@gmail.com>
2023-09-02 15:06:09 +0530

    nav update 2

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,27 +1,28 @@
-nav = `
+nav = ` <header>
+
 <div class="header">
-<a href="../index.html" class="logo"><img src="../src/logo.png" alt="Logo" class="logoimg"></a>
-<input type="checkbox" id="check">
-<label for="check" class="icons">
-    <i class='bx bx-menu' id="menu-icon"></i>
-    <i class='bx bx-x' id="close-icon"></i>
-</label>
-<a href ="../index.html" id="iete">IETE</a>
-<p id="ietename"><b>The Institution of Electronics and Telecommunication Engineers,<br>Chennai</b></p>
+    <a href="../index.html" class="logo"><img src="../src/logo.png" alt="Logo" class="logoimg"></a>
+    <input type="checkbox" id="check">
+    <label for="check" class="icons">
+        <i class='bx bx-menu' id="menu-icon"></i>
+        <i class='bx bx-x' id="close-icon"></i>
+    </label>  
+    <!-- Navigation Bar -->
+    <nav class="navbar">
+                <a href="../index.html">Home</a>
+                <a href="./about.html">About</a>
+                <a href="./execommit.html">Executive Committee</a>
+                <a href="./events.html">Events</a>
+                <a href="./membership.html">Membership</a>
+                <a href="./chencon.html">Chencon</a>
+                <a href="#">Chenlink Newsletter</a>
+                <a href="./contact.html">Contact</a>
+            </nav>      
+    </div>
+<!--<img src="./src/homephoto.jpg" alt="homepage photo" class="homephoto">-->
 
-<!-- Navigation Bar -->
-<nav class="navbar">
-            <a href="../index.html">Home</a>
-            <a href="./about.html">About</a>
-            <a href="./execommit.html">Executive Committee</a>
-            <a href="./events.html">Events</a>
-            <a href="./membership.html">Membership</a>
-            <a href="./chencon.html">Chencon</a>
-            <a href="#">Chenlink Newsletter</a>
-            <a href="./contact.html">Contact</a>
-        </nav>      
-</div>
 
+</header>
 `
 
 document.write(nav)
