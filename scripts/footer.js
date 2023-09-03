ad46fda60dd315ac14ddbee6d20d7305c4fa7c6b
FEELTHEVENOM <rahul.valli2003@gmail.com>
2023-09-03 16:47:18 +0530

    footer and nav

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,38 +1,47 @@
-foot = `<footer class="footer" style="margin-top: 50px;">
-<div class="container">
-    <h1 style="color: white;">IETE Chennai</h1>
-    <div class="row">
-        <div class="footer-col">
-          
-            <h4>Address:</h4>
+foot = `<div class="container">
+<footer class="py-5" style="padding: 40px;">
+  <div class="row">
+    <div class="col-6 col-md-2 mb-3">
+      <h5>Address:</h5>
             <p>IETE Chennai Centre,<br> No. 37,
             Conran Smith Road Entrance Peters Road,<br>
             Gopalapuram, Chennai - 600 086,<br> Tamil Nadu, India.<br> <br>
             Phone: 044 2835 0773 <br>
-            Email: ietechennai@gmail.com <br></p><br>
-            
+            Email: ietechennai@gmail.com <br></p>
+    </div>
+
+    <div class="col-6 col-md-2 mb-3">
+      <h5>Courses:</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">VLSI</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">5G</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Quantum Computing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Communication</a></li>
+      </ul>
+    </div>
+
+
+    <div class="col-md-5 offset-md-1 mb-3">
+      <form>
+        <h5>Subscribe to our newsletter</h5>
+        <p>Monthly digest of what's new and exciting from us.</p>
+        <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+          <label for="newsletter1" class="visually-hidden">Email address</label>
+          <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
+          <button class="btn btn-primary" type="button">Subscribe</button>
         </div>
-        
-        <div class="footer-col">
-            <h4>Courses:</h4>
-            <ul>
-                <li><a href="#">VLSI</a></li>
-                <li><a href="#">5G</a></li>
-                <li><a href="#">Quantum Computing</a></li>
-                <li><a href="#">Communication</a></li>
-            </ul>
-        </div>
-        <div class="footer-col">
-            <h4>Follow us:</h4>
-            <div class="social-links">
-                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-                <a href="#"><i class="fab fa-linkedin-in"></i></a>
-            </div>
-        </div>
-    </div><br>
-    <p style="color:rgb(119, 119, 118);">© Copyright IETE Chennai. All Rights Reserved</p>
-</div>
-</footer>`
+      </form>
+    </div>
+  </div>
+
+  <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+    <p>© Copyright IETE Chennai. All Rights Reserved.</p>
+    <ul class="list-unstyled d-flex">
+      <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+      <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+      <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+    </ul>
+  </div>
+</footer>
+</div>`
 document.write(foot)
