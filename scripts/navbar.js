6e9982b7a52837cc148c02e91221d76b8432213a
Sameekshasa <sameeksha160502@gmail.com>
2023-09-03 18:26:39 +0530

    ahsg

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -20,13 +20,13 @@ nav = `<header class="header">
                 <a class="nav-link" href="./about.html">About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="./Executive commit.html">Exe committee</a>
+                <a class="nav-link" href="./execommit.html">Executive committee</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="./events.html">Events</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="./navbar/membership.html">Membership</a>
+                <a class="nav-link" href="./membership.html">Membership</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
